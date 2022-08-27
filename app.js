c6e00d4fdab127f2c94a38859ccd5a52eefbba33
Jacek Grochowina <jacek99@vp.pl>
2022-08-27 12:47:19 +0200

    move database file & clear code

--- app.js
+++ app.js
@@ -6,27 +6,17 @@ const Router = require("./routes/routes");
 require("dotenv/config");
 
 /*
- * Express
+ * Express & bodyParser
  *
  */
 const app = express(); // init express
 app.use(express.json()); // use express json
-
-// ---------------------------------
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
 app.use(bodyParser.json());
-// ---------------------------------
-// app.use(bodyParser.urlencoded());
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// );
-// ---------------------------------
 app.use(cors()); // use cors
 
 /*
@@ -37,8 +27,6 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}. (http://localhost:${PORT}/)`);
 });
-
-app.use(bodyParser.json());
 
 /*
  * Use router
@@ -51,7 +39,7 @@ app.use(Router);
  *
  */
 const connectionHandlers = require("./utils/connectionHandlers");
-const mysqlConnection = require("./utils/database");
+const mysqlConnection = require("./config/database");
 mysqlConnection.connect((error) => {
   !error ? connectionHandlers.success() : connectionHandlers.error();
 });
